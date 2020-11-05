20d624215d5737ea339c26c7310b32d6374518e9
coll1234567 <joshl5324@gmail.com>
2020-11-05 17:49:32 -0500

    Refactor

--- src/main/java/me/jishunamatata/modularhomes/ModularHomes.java
+++ src/main/java/me/jishunamatata/modularhomes/ModularHomes.java
@@ -9,7 +9,8 @@ public class ModularHomes extends ModularPlugin {
 	private final SimpleSemVersion MIN_CORE_VERSION = SimpleSemVersion.fromString("1.0.1");
 
 	public void onEnable() {
-		DatabaseManager.executeUpdate(this, "CREATE TABLE IF NOT EXISTS 'test' ('col' VARCHAR(16) NOT NULL);");
+		DatabaseManager.executeUpdate(this, "CREATE TABLE IF NOT EXISTS 'test' ('col' VARCHAR(16) NOT NULL);",
+				DatabaseManager.getConnection(this), true);
 	}
 
 	@Override
